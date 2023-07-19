d2cbb17f0152251fb1454007fd663c8e3d3eb29b
Leandro Luna <leandro.j.luna@gmail.com>
2023-07-19 00:35:28 -0300

    test: remove unnecessary tests

--- sum.go
+++ sum.go
@@ -10,14 +10,14 @@ func sum(a, b int) int {
 	return a + b
 }
 
-func sub(a, b int) int {
-	return a - b
-}
+// func sub(a, b int) int {
+// 	return a - b
+// }
 
-func mul(a, b int) int {
-	return a * b
-}
+// func mul(a, b int) int {
+// 	return a * b
+// }
 
-func div(a, b int) int {
-	return a / b
-}
+// func div(a, b int) int {
+// 	return a / b
+// }
